7aefaaf90874cf1ff690a054a238486ff8475639
javier edgardo ciceri <javibishop@gmail.com>
2017-11-16 20:29:04 -0300

    token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { PedidosService} from './services/pedidos.service';
 import { FormasEnvioService} from './services/formasenvio.service';
 import { FormasPagosService} from './services/formaspagos.service';
 import { EstadosService } from './services/estados.service';
+import {ActivatedRoute} from '@angular/router';
+import { AppGlobals } from "./app.global"
 
 @Component({
   selector: 'app-root',
@@ -12,4 +14,10 @@ import { EstadosService } from './services/estados.service';
 })
 export class AppComponent {
   title = 'Manejo de Pedidos ML';
+  constructor(private route:ActivatedRoute, private global: AppGlobals){
+    this.route.queryParams.subscribe(params => {
+      // Defaults to 0 if no query param provided.
+      this.global.token = params['access_token'] || '';
+      });
+  }
 }
