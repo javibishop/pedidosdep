6df9a01afe68e6161d2a072c7108b540a6ce4fac
javier edgardo ciceri <javibishop@gmail.com>
2017-12-30 16:06:00 -0300

    vacio token en global

--- src/app/app.global.ts
+++ src/app/app.global.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AppGlobals {
-     token: string = 'APP_USR-6048120304954368-100811-2c7f133a8d06431b3b8c6b621634828b__K_A__-172177242';
+     token: string = '';
      serviceurl = 'https://millped.herokuapp.com/';
 	 curf='';
 	idmercadolibre = '172177242';
